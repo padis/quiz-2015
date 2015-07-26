bc7bf65a93a31e93428c56313ec68bc4720ee6c0
padis <padiss@gmail.com>
2015-07-26 22:44:25 +0200

    lista de preguntas y autoload

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,18 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-/* GET home page. */
+// Pagina de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question)
-router.get('/quizes/answer', quizController.answer)
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload quizId
+
+//Definición de las rutas de /quizes
+router.get('/quizes', quizController.index)
+router.get('/quizes/:quizId(\\d+)', quizController.show)
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer)
 router.get('/quizes/author', quizController.author)
 
 module.exports = router;
