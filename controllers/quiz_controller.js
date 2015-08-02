ecde4c93fefd640bfb1516a1e31d87fb44f5a2f8
padis <padiss@gmail.com>
2015-08-03 01:47:31 +0200

    borrar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -90,6 +90,15 @@ exports.update = function(req, res) {
 	);	
 };
 
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+	req.quiz.destroy().then(
+		function(){
+			res.redirect('/quizes');
+		}
+	).catch(function(error){next(error);});
+};
+
 exports.author = function(req, res) {
   res.render('quizes/author');
 };
--- routes/index.js
+++ routes/index.js
@@ -20,5 +20,6 @@ router.post('/quizes/create', quizController.create);
 router.get('/quizes/author', quizController.author);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
+router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
 
 module.exports = router;
